ededab6d1b1399966ce8be2a163753e6c7c79d36
luciladayan <48716397@est.ort.edu.ar>
2024-06-03 09:03:30 -0300

    Vehiculo en alquiler

---
+++ Assets/VEHICULOSMANAGER.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VEHICULOSMANAGER : MonoBehaviour
+{
+
+    public Vehiculo VehiculoScript;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int cantRuedas = VehiculoScript.cantRuedas;
+        Debug.Log("Cant ruedas: " + cantRuedas);
+        if (cantRuedas < 6) ;
+
+        {
+            VehiculoScript.gameObject.SetActive(false);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
